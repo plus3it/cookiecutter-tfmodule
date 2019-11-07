30cc1a27201670bb9baf91e71a7d881b3dbfea88
confusdcodr <confusdcodr@users.noreply.github.com>
2019-11-07 11:16:38 -0500

    Update testing harness to check for tf files

--- {{cookiecutter.module_name}}/tests/module_test.go
+++ {{cookiecutter.module_name}}/tests/module_test.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
@@ -19,20 +20,38 @@ func TestModule(t *testing.T) {
 	for _, f := range files {
 		// look for directories with test cases in it
 		if f.IsDir() && f.Name() != "vendor" {
-			t.Run(f.Name(), func(t *testing.T) {
-				// check if a prereq directory exists
-				prereqDir := f.Name() + "/prereq/"
-				if _, err := os.Stat(prereqDir); err == nil {
-					prereqOptions := createTerraformOptions(prereqDir)
-					defer terraform.Destroy(t, prereqOptions)
-					terraform.InitAndApply(t, prereqOptions)
-				}
+			tfFiles, tfErr := ioutil.ReadDir(f.Name())
 
-				// run terraform code for test case
-				terraformOptions := createTerraformOptions(f.Name())
-				defer terraform.Destroy(t, terraformOptions)
-				terraform.InitAndApply(t, terraformOptions)
-			})
+			if tfErr != nil {
+				log.Fatal(tfErr)
+			}
+
+			// check if directory contains terraform files
+			terraformDir := false
+			for _, tf := range tfFiles {
+				if strings.HasSuffix(tf.Name(), ".tf") {
+					terraformDir = true
+					break
+				}
+			}
+
+			// create a test for each directory with terraform files in it
+			if terraformDir {
+				t.Run(f.Name(), func(t *testing.T) {
+					// check if a prereq directory exists
+					prereqDir := f.Name() + "/prereq/"
+					if _, err := os.Stat(prereqDir); err == nil {
+						prereqOptions := createTerraformOptions(prereqDir)
+						defer terraform.Destroy(t, prereqOptions)
+						terraform.InitAndApply(t, prereqOptions)
+					}
+
+					// run terraform code for test case
+					terraformOptions := createTerraformOptions(f.Name())
+					defer terraform.Destroy(t, terraformOptions)
+					terraform.InitAndApply(t, terraformOptions)
+				})
+			}
 		}
 	}
 }
